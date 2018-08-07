57e46c4d0c2fd9c10b70e938b44a82cdeaf88986
Pamela Bortnick <pbortnick@gmail.com>
2018-08-07 22:58:02 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,37 +32,37 @@ end
 
 
 
-# # def display_board(board)
-# #   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-# #   puts "-----------"
-# #   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-# #   puts "-----------"
-# #   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-# # end
-
-# def valid_move?(board, index)
-#   if index >= 0 && index <= 8 && board[index] ==  " "
-#     true
-#   else
-#     false
-#   end
+# def display_board(board)
+#   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+#   puts "-----------"
+#   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+#   puts "-----------"
+#   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 # end
 
+def valid_move?(board, index)
+  if index >= 0 && index <= 8 && board[index] ==  " "
+    true
+  else
+    false
+  end
+end
 
-# def position_taken?(board, index)
-#   if board[index] ==  " " || board[index] == ""
-#     false
-#   elsif board[index] == nil
-#     false
-#   elsif board[index] == "X" || board[index] == "O"
-#     true
-#   end
-# end
 
-# def input_to_index(input)
-#   input.to_i - 1
-# end
+def position_taken?(board, index)
+  if board[index] ==  " " || board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
 
-# def move(array, index, value = "X")
-#   array[index] = value
-# end
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(array, index, value = "X")
+  array[index] = value
+end
