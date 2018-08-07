f88f03dccd189cd55c7a1618a09b2e0954a6cde0
Pamela Bortnick <pbortnick@gmail.com>
2018-08-07 22:51:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,17 +1,3 @@
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index, value = "X")
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
-
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -20,29 +6,69 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board, index)
-  if index >= 0 && index <= 8 && board[index] ==  " "
-    true
-  else
-    false
-  end
-end
 
 
-def position_taken?(board, index)
-  if board[index] ==  " " || board[index] == ""
-    false
-  elsif board[index] == nil
-    false
-  elsif board[index] == "X" || board[index] == "O"
-    true
-  end
-end
 
-def input_to_index(input)
-  input.to_i - 1
-end
 
-def move(array, index, value = "X")
-  array[index] = value
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def turn(board)
+#   puts "Please enter 1-9:"
+#   input = gets.strip
+#   index = input_to_index(input)
+#   if valid_move?(board, index)
+#     move(board, index, value = "X")
+#     display_board(board)
+#   else
+#     turn(board)
+#   end
+# end
+
+
+
+# # def display_board(board)
+# #   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+# #   puts "-----------"
+# #   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+# #   puts "-----------"
+# #   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+# # end
+
+# def valid_move?(board, index)
+#   if index >= 0 && index <= 8 && board[index] ==  " "
+#     true
+#   else
+#     false
+#   end
+# end
+
+
+# def position_taken?(board, index)
+#   if board[index] ==  " " || board[index] == ""
+#     false
+#   elsif board[index] == nil
+#     false
+#   elsif board[index] == "X" || board[index] == "O"
+#     true
+#   end
+# end
+
+# def input_to_index(input)
+#   input.to_i - 1
+# end
+
+# def move(array, index, value = "X")
+#   array[index] = value
+# end
